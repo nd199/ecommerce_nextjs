b2a40e19b06759db8b0e0bc4731b011170fff7be
Narendran <naren06251999@gmail.com>
2024-11-16 08:26:27 +0530

    Changes in css Navbar

--- src/lib/db.js
+++
@@ -1,9 +0,0 @@
-// src/lib/db.js
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, 
-});
-
-const query = (text, params) => pool.query(text, params);
-module.exports = { query };
